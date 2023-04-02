9047293965141899252ff52d7348708ce27dffff
aquila-shivam <72098611+aquila-shivam@users.noreply.github.com>
2023-04-03 04:34:32 +0530

    post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Form from "@/components/Form"
 import Header from "@/components/Header"
+import PostFeed from "@/components/posts/PostFeed"
 export default function Home() {
   return (
     <>
       <div className="text-3xl text-sky-500">
-        <Header showBackArrow label ="Home"/>
+        <Header label ="Home"/>
+        <Form placeholder ="What's happening?"/>
+        <PostFeed/>
       </div>     
     </>
   )
